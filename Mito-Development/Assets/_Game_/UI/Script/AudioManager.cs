13f5ed1b326816b748f78013b75c65e88be85fc9
Joar <crisostomo.mavijoar2003@gmail.com>
2024-01-05 20:01:34 +0800

    Dropdown music, Volume Settings Added

--- Mito-Development/Assets/_Game_/UI/Script/AudioManager.cs
+++ Mito-Development/Assets/_Game_/UI/Script/AudioManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
@@ -8,7 +10,9 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource musicSource;
     [SerializeField] private AudioSource SFXSource;
 
-    public AudioClip background;
+    public AudioClip defaultBackground;
+    public AudioClip track1Background;
+    public AudioClip track2Background;
     public AudioClip buttonClick;
     public AudioClip quitButtonClick;
     public AudioClip playButtonClick;
@@ -18,6 +22,7 @@ public class AudioManager : MonoBehaviour
     public AudioClip infoClick;
     public AudioClip nextClick;
     public AudioClip prevClick;
+    [SerializeField] Dropdown dropDown;
 
 
     public static AudioManager instance;
@@ -31,12 +36,39 @@ public class AudioManager : MonoBehaviour
         }
     }
     private void Start() {
-        musicSource.clip = background;
-        musicSource.Play();
+        int track = PlayerPrefs.GetInt("Track");
+            OnValueChanged(0);
     }
-    
+
+    public void OnValueChanged(int val)
+    {
+        switch (val)
+        {
+            case 0:
+            PlayerPrefs.SetInt("Track", val);
+            musicSource.clip = defaultBackground;
+            musicSource.Play();
+            break;
+            case 1:
+            PlayerPrefs.SetInt("Track", val); 
+            musicSource.clip = track1Background;
+            musicSource.Play();
+            break;
+            case 2: 
+            PlayerPrefs.SetInt("Track", val);
+            musicSource.clip = track2Background;
+            musicSource.Play();
+            break;
+        }
+        
+    }
+
     public void PlaySFX(AudioClip clip){
         SFXSource.PlayOneShot(clip);
     }
+    void PlayMusic(){
+
+    }
+    
 
 }
